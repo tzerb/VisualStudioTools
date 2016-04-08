8ad9b5fca61045c68cee03ccda1f26419988632b
Tim Zirbel (eaa) <tzirbel@eaa.org>
2016-04-08 18:02:49 -0500

    Started choco installs for new machine.

---
+++ Choco/new machine.bat
@@ -0,0 +1,12 @@
+choco install -y googlechrome  
+choco install -y firefox 
+
+choco install -y autohotkey.install 
+
+choco install -y nodejs.install 
+choco install -y notepadplusplus.install 
+choco install -y git.install
+choco install -y git 
+choco install -y gitextensions 
+choco install -y powershell 
+choco install -y fiddler 
