8a6dfaa377b2d5a9a5f0a95f0a32d419becdd40e
Tim Zirbel <tzirbel@eaa.org>
2016-05-16 14:56:38 -0500

    Progress

--- Watin Code Gen/InsuranceAppWebUI/ObituaryAddTests.cs
+++ Watin Code Gen/InsuranceAppWebUI/ObituaryAddTests.cs
@@ -8,21 +8,6 @@ using InsuranceAppWebUI.Model;
 
 namespace InsuranceAppWebUI
 {
-    public class TestBase
-    {
-        private static string _serverAddress = "http://dev.eaa.org";
-
-        public string TestImage()
-        {
-            return @"C:\Users\tzirbel\Pictures\Sky2.jpg";
-        }
-
-        public IE BrowseTo(string url)
-        {
-            return new IE(string.Format("{0}{1}", _serverAddress, url));
-        }
-    }
-
     [TestClass]
     public class ObituaryAddTests : TestBase
     {
---
+++ Watin Code Gen/InsuranceAppWebUI/TestBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WatiN.Core;
+
+namespace InsuranceAppWebUI
+{
+    public class TestBase
+    {
+        private static string _serverAddress = "http://dev.eaa.org";
+
+        public string TestImage()
+        {
+            return @"C:\Users\tzirbel\Pictures\Sky2.jpg";
+        }
+
+        public IE BrowseTo(string url)
+        {
+            return new IE(string.Format("{0}{1}", _serverAddress, url));
+        }
+    }
+}
